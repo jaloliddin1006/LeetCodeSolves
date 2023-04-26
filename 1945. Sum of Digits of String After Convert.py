01dab027aa0c8a34760a4d148c854d50347b2ac3
Jaloliddin <jmamatmusayev@gmail.com>
2023-04-26 23:47:07 +0500

    Create 1945. Sum of Digits of String After Convert.py

---
+++ 1945. Sum of Digits of String After Convert.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        self.s = s
+        self.k = k
+
+        alpha = "abcdefghijklmnopqrstuvwxyz"
+        s_num = [lambda arg=x: alpha.index(arg)+1 for x in s]
+        my_list = ''
+        for n in s_num:
+                my_list += str(n())
+        for i in range(k):
+            b = 0
+            for i in str(my_list):
+                 b += int(i)
+            my_list = b
+            
+            
+        return my_list
+
+
+
+
+a = Solution()
+print(a.getLucky('iiii',1))
+
+#out: 36

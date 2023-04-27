15847520d44ac87118e86113be5e283475688878
Jaloliddin <jmamatmusayev@gmail.com>
2023-04-27 23:02:10 +0500

    Create 66. Plus One.py

---
+++ 66. Plus One.py
@@ -0,0 +1,14 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        self.digits = digits
+        a = ''
+        out = []
+        for i in digits:
+            a += str(i)
+        for j in str(int(a)+1):
+            out.append(int(j))
+        return out
+
+
+a = Solution()
+print(a.plusOne([4,3,2,1]))

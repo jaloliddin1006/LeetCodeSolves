a36ea2ba85c9907c3c84ba6f228af4db9de95c88
Jaloliddin <jmamatmusayev@gmail.com>
2023-05-09 23:57:28 +0500

    Update 2133. Check if Every Row and Column Contains All Numbers.py

--- 2133. Check if Every Row and Column Contains All Numbers.py
+++ 2133. Check if Every Row and Column Contains All Numbers.py
@@ -2,8 +2,8 @@ class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         rows = collections.defaultdict(set)
         colls = collections.defaultdict(set)
-        for r in range(3):
-            for c in range(3):
+        for r in range(len(matrix)):
+            for c in range(len(matrix)):
                 if (matrix[r][c] in rows[r] or matrix[r][c] in colls[c]):
                     return False
                 rows[r].add(matrix[r][c]) 

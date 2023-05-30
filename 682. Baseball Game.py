4c91c076f12a7e8233208a6c29784b13dcda2adf
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2023-05-30 19:55:52 +0500

    Create 682. Baseball Game.py

---
+++ 682. Baseball Game.py
@@ -0,0 +1,28 @@
+class Solution:
+    def calPoints(self, operations: list[str]) -> int:
+        score = []
+        n = 0
+        for i in operations:
+            
+            if i == "D":
+                score.append(score[n-1]*2)
+                n += 1
+                continue
+            if i == "C":
+                score.pop()
+                n -= 1
+                continue
+            if i == "+":
+                score.append(score[n-1]+score[n-2])
+                n += 1
+                continue
+        
+            score.append(int(i))
+            n += 1
+        return sum(score)
+    
+    
+print(Solution().calPoints(["5","-2","4","C","D","9","+","+"]))
+
+
+print("-1".isdigit())

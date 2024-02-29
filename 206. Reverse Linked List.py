3db7bfda02df277aed08e3e5294d38c58fdeb73e
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2024-02-29 07:20:22 +0500

    Update 206. Reverse Linked List.py

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -1,7 +1,7 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         prev = None
